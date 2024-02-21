7eec8916702feb1d4bb491edda8f6af0a862aad5
Arpit Goyal <arpitgoyal.iitkgp@gmail.com>
2024-02-21 21:52:39 +0530

    fixed the alignment of github stats

--- src/components/Stats/Github.js
+++ src/components/Stats/Github.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const GithubStats = () => (
   <>
-    <img align="center" src="https://github-readme-stats.vercel.app/api?username=argoyal&count_private=true&show_icons=true&include_all_commits=true&theme=light" alt="" height="200px" width="1000px" /><br />
-    <img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=argoyal&theme=light" alt="Arpit's LangStat" height="200px" width="1000px" /><br />
-    <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=argoyal&langs_count=10&show_icons=true&locale=en&layout=compact&theme=light" alt="Arpit's language" height="200px" width="1000px" />
+    <div>
+      <img align="center" src="https://github-readme-stats.vercel.app/api?username=argoyal&count_private=true&show_icons=true&include_all_commits=true&theme=light" alt="" height="200px" width="600px" /><br />
+      <img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=argoyal&theme=light" alt="Arpit's LangStat" height="200px" width="600px" /><br />
+      <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=argoyal&langs_count=10&show_icons=true&locale=en&layout=compact&theme=light" alt="Arpit's language" height="200px" width="600px" />
+    </div>
   </>
 );
 
