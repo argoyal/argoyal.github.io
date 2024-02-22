b66f9dcb86dac30a2017475799d9b7ab2a069b7b
Arpit Goyal <10412881+argoyal@users.noreply.github.com>
2024-02-22 11:04:24 +0530

    updated the text in index.js page.

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,9 +14,8 @@ const Index = () => (
             <b>Check out the latest acquisition news <a href="https://economictimes.indiatimes.com/tech/startups/ey-india-acquires-ai-startup/articleshow/79218623.cms" target="_blank" rel="noreferrer"> here </a></b>
           </p>
           <p>
-            I am a Python professional with the focus on the usage of open source technologies.
-            I also have an extensive understanding of systems architecture.
-            Analysing real-world problems, curating solutions and implementing them through
+            I am a DevOps professional with an extensive understanding of systems architecture.
+            Analyzing real-world problems, curating solutions, and implementing them through
             the digital platform is my passion.
           </p>
         </div>
